8d55f49fd8519f495c34f809f4b015aef052956c
Михаил Проказин <bearpro@outlook.com>
2025-10-20 04:59:04 +0300

    Enhance error output in `validate` command

--- ddl-ltlf/src/Mprokazin.DdlLtlf.Cli/Commands/Validate.fs
+++ ddl-ltlf/src/Mprokazin.DdlLtlf.Cli/Commands/Validate.fs
@@ -25,14 +25,47 @@ let createInputs (paramters: ValidateParameters) =
           Reader = System.Console.In }
     ]
 
+type AntlrError = {
+    Line: int
+    Char: int
+    Message: string
+}
+
 type ValidationError =
     | SemanticError of Mprokazin.DdlLtlf.Language.Semantics.SemanticError
+    | LexerError of AntlrError
+    | ParserError of AntlrError
     | Exception of exn
+
+type CollectingErrorListener() =
+    inherit Antlr4.Runtime.BaseErrorListener()
+
+    let mutable errors: AntlrError list = []
+
+    member _.Errors = errors |> List.rev
+
+    override _.SyntaxError(output, recognizer, offendingSymbol, line, charPositionInLine, msg, e) =
+        let e = { 
+            Line = line
+            Char = charPositionInLine
+            Message = msg }
+        errors <- e :: errors
+
+    interface Antlr4.Runtime.IAntlrErrorListener<int> with 
+        member this.SyntaxError (
+            output: TextWriter, 
+            recognizer: Antlr4.Runtime.IRecognizer, 
+            offendingSymbol: int, 
+            line: int, 
+            charPositionInLine: int, 
+            msg: string, 
+            e: Antlr4.Runtime.RecognitionException): unit = 
+                this.SyntaxError(output, recognizer, null, line, charPositionInLine, msg, e)
 
 type ValidationResult = { 
     Source: string
     Errors: ValidationError list
-    Model: obj option
+    Model: Mprokazin.DdlLtlf.Language.Ast.Model option
 }
 
 let processInput (input: ValidationInput) = 
@@ -46,19 +79,48 @@ let processInput (input: ValidationInput) =
         try
             let stream = Antlr4.Runtime.AntlrInputStream input.Reader
             let lexer = Mprokazin.DdlLtlf.Language.Antlr.DdlLtlfLexer(stream)
+            let lexerErrors = CollectingErrorListener()
+            lexer.RemoveErrorListeners()
+            lexer.AddErrorListener lexerErrors
+
             let tokens = Antlr4.Runtime.CommonTokenStream(lexer)
             let parser = Mprokazin.DdlLtlf.Language.Antlr.DdlLtlfParser(tokens)
+            let parserErrors = CollectingErrorListener()
+            parser.RemoveErrorListeners()
+            parser.AddErrorListener parserErrors
             
             let tree = parser.root()
-
             let ast = Mprokazin.DdlLtlf.Language.Ast.Visitors.visit tree
-            let semanticValidationResult = Mprokazin.DdlLtlf.Language.Semantics.validate ast
-            
-            match semanticValidationResult with
-            | Ok () -> { result with Model = Some ast }
-            | Error errors -> { result with Errors = result.Errors @ List.map SemanticError errors }
+
+            let parserErrors = 
+                parserErrors.Errors 
+                |> List.map ParserError
+
+            let lexerErrors = 
+                lexerErrors.Errors
+                |> List.map LexerError
+
+            let result = 
+                { result with 
+                    Errors = result.Errors @ lexerErrors @ parserErrors
+                    Model = Some ast }
+
+            result
         with
             | e -> { result with Errors = Exception e :: result.Errors }
+
+    let result = 
+        if Option.isSome result.Model then 
+            try 
+                let semanticValidationResult = Mprokazin.DdlLtlf.Language.Semantics.validate result.Model.Value
+            
+                match semanticValidationResult with
+                | Ok () -> result
+                | Error errors -> { result with Errors = result.Errors @ List.map SemanticError errors }
+            with 
+                | e -> { result with Errors = Exception e :: result.Errors }
+        else result
+        
     result
 
 
@@ -80,23 +142,27 @@ let run (parameters: ValidateParameters) =
             for i in inputs do
                 i.Reader.Dispose()
         
+    let mutable code = 0
+
     for r in results do
         if not (List.isEmpty r.Errors) then do
             printfn "Errors in %s:" r.Source
             for e in r.Errors do
                 match e with
                 | SemanticError e -> 
-                    let location = 
-                        e.Path
-                        |> List.fold (fun s x -> $"{s}->{x}") r.Source
-                    printfn "%s in %s: %s" e.Kind location e.Message
+                    let location = String.concat "->" e.Path
+                    printfn "Semantic error in %s (%s): %s" r.Source location e.Message
+                | LexerError e ->
+                    printfn "Lexer error in %s:line %d:%d: %s" r.Source e.Line e.Char e.Message
+                | ParserError e ->
+                    printfn "Parser error in %s:line %d:%d: %s" r.Source e.Line e.Char e.Message
                 | Exception e ->
-                    printfn "Exception in %s: %s" r.Source e.Message
-            failwith "Validation failed"
+                    printfn "Exception in %s: %s\n%s" r.Source e.Message e.StackTrace
+            code <- 1
 
     if parameters.PrintModel then 
         for r in results do
             if List.isEmpty r.Errors then do
                 printfn "Model in %s:\n%A" r.Source r.Model.Value
     
-    0
+    code
